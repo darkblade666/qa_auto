b36537afd58d9e295c0e5e2c2fff14b78adc09b4
Notebook <vlad.mishaniy@gmail.com>
2015-11-10 22:33:21 +0200

    debug class Figure

--- src/main/java/vlad_mishaniy/com/vk/lesson2/Figure.java
+++ src/main/java/vlad_mishaniy/com/vk/lesson2/Figure.java
@@ -6,25 +6,19 @@ import java.util.Scanner;
  * Created by kip_notebook on 09.11.2015.
  */
 public class Figure {
-    public int i = 0;
-    public int j = 0;
+//    public int i = 0;
+//    public int j = 0;
 
     public static void main (String ... args){
         System.out.println("Enter top of triangular, for example 5");
         Scanner scanner = new Scanner(System.in);
-        int x = scanner.nextInt();
+        int x = scanner.nextInt(), n = 0;
         boolean b = true;
 
-        for(byte i = 0 ; i<=x ; i++ ){
-            for (byte j=0; b ? (j<i) : (j>i); ){
-                j++;
-                if(i==x){
-                    x = x+3;
-                    b = false;
-                }
-                if (!b){
-                    j--;
-                }
+        for(byte i = 0; i<=(x*2) ; i++ ){
+
+            for(byte j = 0; b ? j<=i : j<=((x*2)-i); j++){
+                if (j==x) b=false;
                 System.out.print("*");
             }
             System.out.println("*");
